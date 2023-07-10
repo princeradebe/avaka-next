d5c3bc7764737d8a1aabd4d368ecdf18c7d8e412
Prince Radebe <princeradebe@gmail.com>
2023-07-10 23:55:09 +0200

    Removed background color

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Nav />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}</body>
     </html>
   )
 }
